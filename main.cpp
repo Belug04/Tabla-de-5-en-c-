8c35a7f9193f99b19dc4d008f5ea43332a105270
Belug04 <166060127+Belug04@users.noreply.github.com>
2024-05-16 23:49:56 -0300

    tabla

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+
+
+    for( int ivalor=0; ivalor<=10; ivalor++)
+    {
+
+        cout<< ivalor << " x 5 = " << ivalor * 5<<endl;
+    }
+    return 0;
+}
